c40b18a987a5e5b498d7e77e6d9e56e793b11248
Carlos <chdezmar@gmail.com>
2018-09-07 13:52:49 +0200

    Add option to pass survey structure and survey response. Useful when doing a big import so you don't need to do so many calls to the API One call to the survey details and a bulk call to the survey responses can get up to 100 matched survey_responses.

--- lib/surveymonkey_api/response.rb
+++ lib/surveymonkey_api/response.rb
@@ -2,16 +2,22 @@ module Surveymonkey
   # Response
   class Response
     extend Forwardable
-    attr_reader :survey_id, :id, :survey_structure
+    attr_reader :survey_id, :id, :survey_response
 
-    def initialize(survey_id, id)
+    def initialize(survey_id, id, survey_structure = nil, survey_response = nil)
       @survey_id = survey_id
       @id = id
-      @survey_structure = Surveymonkey::Survey.new(survey_id)
+      @survey_response = survey_response
+      @survey_structure = survey_structure
+      @survey = Surveymonkey::Survey.new(survey_id)
+    end
+
+    def survey_structure
+      @survey_structure ||= details
     end
 
     def raw_details(options = {})
-      @raw_details ||= client.survey_response(survey_id, id, options)
+      @raw_details ||= survey_response || client.survey_response(survey_id, id, options)
     end
 
     def info
@@ -21,13 +27,13 @@ module Surveymonkey
     def pages(options = {})
       @pages ||= raw_details(options)['pages'].each_with_object([]) do |page, arr|
         unless page['questions'].empty?
-          page_structure = details['pages'].detect { |p| p['id'] == page['id'] }
+          page_structure = survey_structure['pages'].detect { |p| p['id'] == page['id'] }
           arr << Surveymonkey::Response::Page.new(page, page_structure)
         end
       end
     end
 
-    def_delegators :@survey_structure, :details
+    def_delegators :@survey, :details
 
     private
 
